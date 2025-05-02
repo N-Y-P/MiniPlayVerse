c2ec0e39ae52ff2dd5966b56267531b26abf755e
나연 <99782442+N-Y-P@users.noreply.github.com>
2025-05-02 17:04:44 +0900

    feat : PlayerController
    플레이어 움직임, 마우스 따라서 이미지 flip

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    #region [변수, 할당 모음]
+    protected Rigidbody2D rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    protected Vector2 moveDir = Vector2.zero; //이동하는 방향
+    public Vector2 MovementDirection { get { return moveDir; } }
+
+    protected Vector2 lookDir = Vector2.zero; //바라보는 방향
+    public Vector2 LookDirection { get { return lookDir; } }
+    #endregion
+    protected virtual void Awake()//상속받아 사용할거라서 virtual 많이 쓸예정
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        Rotate(lookDir);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movment(moveDir);
+    }
+
+    //움직임
+    private void Movment(Vector2 direction)
+    {
+        direction = direction * 5f;
+        rigidbody.velocity = direction;
+
+    }
+
+    //회전
+    private void Rotate(Vector2 direction)
+    {
+        //아크탄젠트 *순서확인!
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;//y와 x축사이의 세타값
+        bool isLeft = Mathf.Abs(rotZ) > 90f;//90도보다 크다면 왼쪽을 바라봐
+
+        characterRenderer.flipX = isLeft;
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : BaseController
+{
+    private Camera camera;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        camera = Camera.main;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //base.Update();
+    }
+
+    void OnMove(InputValue input)
+    {
+        moveDir = input.Get<Vector2>();
+        moveDir = moveDir.normalized;
+    }
+    void OnLook(InputValue input)
+    {
+        Vector2 mousePos = input.Get<Vector2>();
+        Vector2 worldPos = (Vector2)camera.ScreenToWorldPoint(mousePos);
+        lookDir = (worldPos - (Vector2)transform.position).normalized;
+    }
+
+}
